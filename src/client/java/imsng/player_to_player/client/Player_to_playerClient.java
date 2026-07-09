ed6689707aee293112ef0818bcd874298a9e07fa
yxw1019 <147071791+yxw1019@users.noreply.github.com>
2026-07-09 14:45:59 +0800

    0.0.2

---
+++ src/client/java/imsng/player_to_player/client/Player_to_playerClient.java
@@ -0,0 +1,10 @@
+package imsng.player_to_player.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Player_to_playerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
